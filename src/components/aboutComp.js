1a8f736b1b5fafcd797309c171e7f708bfc0d5e3
Bagus Rinu Pangayom <bagusrinup@gmail.com>
2023-12-26 21:10:59 +0700

    add: add a new code in aboutComp component 1. function

---
+++ src/components/aboutComp.js
@@ -0,0 +1,11 @@
+const AboutComp = () => {
+    return (
+        <>
+            <div className="mt-5 pt-5">
+                <h1>tes about</h1>
+            </div>
+        </>
+    );
+};
+
+export default AboutComp;
